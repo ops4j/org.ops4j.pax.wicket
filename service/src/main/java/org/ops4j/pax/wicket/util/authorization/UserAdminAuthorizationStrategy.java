9c4143b7c6b8596dc688ef7ab2a05e0e4e724042
ops4j@leangen.net <ops4j@leangen.net@9b982a3c-3ae5-0310-a4bc-d9a3335569bd>
2008-08-01 10:54:04 +0000

    Updates to UserAdminAuthorizationStrategy.
    git-svn-id: https://scm.ops4j.org/repos/ops4j/projects/pax/wicket@12518 9b982a3c-3ae5-0310-a4bc-d9a3335569bd

--- service/src/main/java/org/ops4j/pax/wicket/util/authorization/UserAdminAuthorizationStrategy.java
+++ service/src/main/java/org/ops4j/pax/wicket/util/authorization/UserAdminAuthorizationStrategy.java
@@ -18,19 +18,53 @@ package org.ops4j.pax.wicket.util.authorization;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.authorization.*;
-import org.osgi.service.useradmin.UserAdmin;
+import org.osgi.service.useradmin.*;
 
+/**
+ * Implementation of IAuthorizationStrategy that is backed directly by the
+ * OSGi UserAdmin service. Note that this is a simple and probably incomplete
+ * implementation. Some issues not addressed:
+ * 
+ *   - how do we handle anonymous classes?
+ *   - how do we handle class hierarchies?
+ * 
+ * The approach is to create a {@code Group} with the name of the action to
+ * authorize (or not). This is done by using the Role annotation. Since
+ * there are several actions for each wicket component, this is the 
+ * convention:
+ * 
+ *    NameOfAction --> instantiation of that component
+ *    NameOfAction.ENABLE --> enable for that component
+ *    NameOfAction.RENDER --> render for that component
+ * 
+ * This will of course be expanded if/when the wicket team adds additional
+ * {@code Action}s.
+ * 
+ * As for the actual authorization logic, this is handled normally via
+ * the UserAdmin service.
+ * 
+ * @author David Leangen
+ */
 public class UserAdminAuthorizationStrategy
     implements IAuthorizationStrategy
 {
     private final UserAdmin m_userAdmin;
+
+    /**
+     * The default parameter to use for obtaining a user from the UserAdmin
+     * service. Use like so:
+     * <pre>
+     *     User user = userAdmin.getUser( PAX_WICKET_USER_PARAM, loginName );
+     * </pre>
+     */
+    public static final String PAX_WICKET_USER_PARAM = "wicket.username";
 
     public UserAdminAuthorizationStrategy( UserAdmin userAdmin )
     {
         m_userAdmin = userAdmin;
     }
 
-    public boolean isActionAuthorized( Component component, Action action )
+    public final boolean isActionAuthorized( Component component, Action action )
     {
         final Class< ? extends Component> componentClass = component.getClass();
         final Role roleAnnotation = componentClass.getAnnotation( Role.class );
@@ -40,7 +74,7 @@ public class UserAdminAuthorizationStrategy
         return true;
     }
 
-    public boolean isInstantiationAuthorized( Class componentClass )
+    public final boolean isInstantiationAuthorized( Class componentClass )
     {
         final Role roleAnnotation = (Role)componentClass.getAnnotation( Role.class );
         if ( !isAuthorized( roleAnnotation ) )
@@ -76,6 +110,29 @@ public class UserAdminAuthorizationStrategy
 
     private boolean isAuthorized( String role )
     {
-        return false;
+        // FIXME
+        // This is my only hangup so far... Need to get the name of the
+        // logged in user
+        final String loginName = "";
+        final User user = getUser( m_userAdmin, loginName );
+        final Authorization auth = m_userAdmin.getAuthorization( user );
+        return auth.hasRole( role );
+    }
+
+    /**
+     * Override this method in order to provide a different way
+     * of obtaining the User from the UserAdmin service.
+     * 
+     * @param userAdmin  UserAdmin service
+     * @param loginName  the name under which the user is logged in
+     * 
+     * @return the User object, or {@code null} if no such user
+     */
+    protected User getUser( UserAdmin userAdmin, String loginName )
+    {
+        if( null == loginName )
+            return null;
+
+        return userAdmin.getUser( PAX_WICKET_USER_PARAM, loginName );
     }
 }
