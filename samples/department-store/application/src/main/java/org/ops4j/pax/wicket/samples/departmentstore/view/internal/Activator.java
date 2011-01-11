96ecf52adb3d761258a7df57056f95b202fb1983
Andreas Pieber <anpieber@gmail.com>
2011-01-11 23:54:13 +0100

    [PAXWICKET-40] do a workaround to fix the problem with CSS files
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

--- samples/department-store/application/src/main/java/org/ops4j/pax/wicket/samples/departmentstore/view/internal/Activator.java
+++ samples/department-store/application/src/main/java/org/ops4j/pax/wicket/samples/departmentstore/view/internal/Activator.java
@@ -1,6 +1,7 @@
 /*
  * Copyright 2006 Niclas Hedhman.
  * Copyright 2006 Edward F. Yakop
+ * Copyright 2010 Andreas Pieber
  *
  * Licensed  under the  Apache License,  Version 2.0  (the "License");
  * you may not use  this file  except in  compliance with the License.
@@ -18,20 +19,16 @@
  */
 package org.ops4j.pax.wicket.samples.departmentstore.view.internal;
 
+import static org.apache.wicket.util.lang.Objects.setObjectStreamFactory;
+
 import org.ops4j.pax.wicket.api.PaxWicketApplicationFactory;
 import org.ops4j.pax.wicket.util.RootContentAggregator;
 import org.ops4j.pax.wicket.util.serialization.PaxWicketObjectStreamFactory;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
-import static org.apache.wicket.util.lang.Objects.setObjectStreamFactory;
 
-/**
- * @since 1.0.0
- */
-public class Activator
-    implements BundleActivator
-{
+public class Activator implements BundleActivator {
 
     private RootContentAggregator m_store;
     private ServiceRegistration m_serviceRegistration;
@@ -39,29 +36,26 @@ public class Activator
     private PaxWicketApplicationFactory m_applicationFactory;
     private OverviewPageFactory m_overviewPageFactory;
 
-    public void start( BundleContext bundleContext )
-        throws Exception
-    {
+    public void start(BundleContext bundleContext) throws Exception {
+        // to mount in root context use "/" instead. Please keep in mind that if you change this value you also have to
+        // update the stylesheet link in the OverviewPage.html. E.g. if you change the mount point here to / you have to
+        // change deptStore/stylesheets/style.css in OverviewPage.html to stylesheets/style.css
         String mountPoint = "deptStore";
-//        String mountPoint = "/";
         String applicationName = "departmentstore";
-        setObjectStreamFactory( new PaxWicketObjectStreamFactory( true ) );
-        m_store = new RootContentAggregator( bundleContext, applicationName, "swp" );
+        setObjectStreamFactory(new PaxWicketObjectStreamFactory(true));
+        m_store = new RootContentAggregator(bundleContext, applicationName, "swp");
         m_pageRegistration = m_store.register();
 
-        m_overviewPageFactory = new OverviewPageFactory( bundleContext, m_store, applicationName, "overview" );
+        m_overviewPageFactory = new OverviewPageFactory(bundleContext, m_store, applicationName, "overview");
         m_overviewPageFactory.register();
 
         m_applicationFactory =
-            new PaxWicketApplicationFactory( bundleContext, OverviewPage.class, mountPoint, applicationName );
+            new PaxWicketApplicationFactory(bundleContext, OverviewPage.class, mountPoint, applicationName);
 
         m_serviceRegistration = m_applicationFactory.register();
-
     }
 
-    public void stop( BundleContext bundleContext )
-        throws Exception
-    {
+    public void stop(BundleContext bundleContext) throws Exception {
         m_pageRegistration.unregister();
         m_serviceRegistration.unregister();
         m_overviewPageFactory.dispose();
