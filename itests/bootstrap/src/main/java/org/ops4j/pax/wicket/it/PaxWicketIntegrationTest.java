f40a60927440ef7d97abc6c565f897e14967ef22
Andreas Pieber <anpieber@gmail.com>
2011-05-21 01:00:11 +0200

    [PAXWICKET-98] Fixed dependency issue with jetty-plus and also integrate into integration tests
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

--- itests/bootstrap/src/main/java/org/ops4j/pax/wicket/it/PaxWicketIntegrationTest.java
+++ itests/bootstrap/src/main/java/org/ops4j/pax/wicket/it/PaxWicketIntegrationTest.java
@@ -20,6 +20,7 @@ import static org.junit.Assert.assertNotNull;
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
 import static org.ops4j.pax.exam.CoreOptions.options;
 import static org.ops4j.pax.exam.CoreOptions.provision;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
 
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Option;
@@ -43,8 +44,14 @@ public abstract class PaxWicketIntegrationTest {
                 .versionAsInProject()),
             provision(mavenBundle().groupId("org.apache.felix").artifactId("org.apache.felix.configadmin")
                 .versionAsInProject()),
+            provision(mavenBundle().groupId("org.apache.geronimo.specs").artifactId("geronimo-activation_1.1_spec")
+                    .versionAsInProject()),
             provision(mavenBundle().groupId("org.apache.geronimo.specs").artifactId("geronimo-servlet_2.5_spec")
                 .versionAsInProject()),
+            provision(mavenBundle().groupId("org.apache.geronimo.specs").artifactId("geronimo-jta_1.1_spec")
+                    .versionAsInProject()),
+            provision(mavenBundle().groupId("org.apache.servicemix.bundles")
+                .artifactId("org.apache.servicemix.bundles.javax.mail").versionAsInProject()),
             provision(mavenBundle().groupId("org.apache.servicemix.bundles")
                 .artifactId("org.apache.servicemix.bundles.asm")
                 .versionAsInProject()),
@@ -56,23 +63,27 @@ public abstract class PaxWicketIntegrationTest {
             provision(mavenBundle().groupId("org.eclipse.jetty").artifactId("jetty-security").versionAsInProject()),
             provision(mavenBundle().groupId("org.eclipse.jetty").artifactId("jetty-servlet").versionAsInProject()),
             provision(mavenBundle().groupId("org.eclipse.jetty").artifactId("jetty-xml").versionAsInProject()),
+            provision(mavenBundle().groupId("org.eclipse.jetty").artifactId("jetty-jndi").versionAsInProject()),
+            provision(mavenBundle().groupId("org.eclipse.jetty").artifactId("jetty-plus").versionAsInProject()),
+            provision(mavenBundle().groupId("org.eclipse.jetty").artifactId("jetty-webapp").versionAsInProject()),
             provision(mavenBundle().groupId("org.ops4j.pax.web").artifactId("pax-web-api").versionAsInProject()),
             provision(mavenBundle().groupId("org.ops4j.pax.web").artifactId("pax-web-spi").versionAsInProject()),
             provision(mavenBundle().groupId("org.ops4j.pax.web").artifactId("pax-web-runtime").versionAsInProject()),
             provision(mavenBundle().groupId("org.ops4j.pax.web").artifactId("pax-web-jetty").versionAsInProject()),
             provision(mavenBundle().groupId("org.osgi").artifactId("org.osgi.compendium").versionAsInProject()),
             provision(mavenBundle().groupId("org.ops4j.pax.wicket").artifactId("pax-wicket-service")
-                .versionAsInProject()));
+                .versionAsInProject()),
+            systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("WARN"));
     }
 
     /**
      * Return bundle given the symbolic name. Returns {@code null} if not found.
-     *
+     * 
      * @param bundleContext Bundle context. This argument must not be {@code null}.
      * @param symbolicName Bundle symbolic name. This argument must not be {@code null}.
-     *
+     * 
      * @return The bundle given the symbolic name.
-     *
+     * 
      * @since 0.5.5
      */
     protected final Bundle getBundleBySymbolicName(BundleContext bundleContext, String symbolicName) {
@@ -89,11 +100,11 @@ public abstract class PaxWicketIntegrationTest {
 
     /**
      * Returns the pax wicket service bundle.
-     *
+     * 
      * @param bundleContext Bundle context. This argument must not be {@code null}.
-     *
+     * 
      * @return The pax wicket service bundle.
-     *
+     * 
      * @since 0.5.5
      */
     protected final Bundle getPaxWicketServiceBundle(BundleContext bundleContext) {
