df580def559e31253dda028d5986709eb545cda0
Andreas Pieber <anpieber@gmail.com>
2011-06-05 09:25:02 +0200

    Reformatted pageMounter to make changes visible
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

--- service/src/main/java/org/ops4j/pax/wicket/util/DefaultPageMounter.java
+++ service/src/main/java/org/ops4j/pax/wicket/util/DefaultPageMounter.java
@@ -1,75 +1,63 @@
 package org.ops4j.pax.wicket.util;
 
+import static org.ops4j.lang.NullArgumentException.validateNotEmpty;
+import static org.ops4j.lang.NullArgumentException.validateNotNull;
+
 import java.util.List;
 import java.util.Vector;
+
 import org.apache.wicket.Page;
 import org.apache.wicket.request.target.coding.BookmarkablePageRequestTargetUrlCodingStrategy;
 import org.apache.wicket.request.target.coding.IRequestTargetUrlCodingStrategy;
-import static org.ops4j.lang.NullArgumentException.validateNotEmpty;
-import static org.ops4j.lang.NullArgumentException.validateNotNull;
 import org.ops4j.pax.wicket.api.MountPointInfo;
 import org.ops4j.pax.wicket.api.PageMounter;
 
-public class DefaultPageMounter
-    implements PageMounter
-{
+public class DefaultPageMounter implements PageMounter {
 
-    private final List<MountPointInfo> m_mountPoints;
+    private final List<MountPointInfo> mountPoints;
 
-    public DefaultPageMounter()
-    {
+    public DefaultPageMounter() {
         // Using Vector because it is synchronized
         // But not sure if synchronization is really necessary or not...
-        m_mountPoints = new Vector<MountPointInfo>();
+        mountPoints = new Vector<MountPointInfo>();
     }
 
     /**
      * A convenience method that uses a default coding strategy.
-     *
-     * @param path      the path on which the page is to be mounted
-     * @param pageClass the class to mount on this mount point using the
-     *                  default strategy
+     * 
+     * @param path the path on which the page is to be mounted
+     * @param pageClass the class to mount on this mount point using the default strategy
      */
-    public void addMountPoint( String path, Class<? extends Page> pageClass )
-    {
-        addMountPoint( path, new BookmarkablePageRequestTargetUrlCodingStrategy( path, pageClass, null ) );
+    public void addMountPoint(String path, Class<? extends Page> pageClass) {
+        addMountPoint(path, new BookmarkablePageRequestTargetUrlCodingStrategy(path, pageClass, null));
     }
 
-    public void addMountPoint( String path, IRequestTargetUrlCodingStrategy codingStrategy )
-    {
-        MountPointInfo info = new DefaultMountPointInfo( path, codingStrategy );
-        m_mountPoints.add( info );
+    public void addMountPoint(String path, IRequestTargetUrlCodingStrategy codingStrategy) {
+        MountPointInfo info = new DefaultMountPointInfo(path, codingStrategy);
+        mountPoints.add(info);
     }
 
-    public final List<MountPointInfo> getMountPoints()
-    {
-        return m_mountPoints;
+    public final List<MountPointInfo> getMountPoints() {
+        return mountPoints;
     }
 
-    private static class DefaultMountPointInfo
-        implements MountPointInfo
-    {
+    private static class DefaultMountPointInfo implements MountPointInfo {
+        private final String path;
+        private final IRequestTargetUrlCodingStrategy codingStrategy;
 
-        private final String m_path;
-        private final IRequestTargetUrlCodingStrategy m_codingStrategy;
-
-        private DefaultMountPointInfo( String path, IRequestTargetUrlCodingStrategy codingStrategy )
-        {
-            validateNotEmpty( path, "path" );
-            validateNotNull( codingStrategy, "codingStrategy" );
-
-            m_path = path;
-            m_codingStrategy = codingStrategy;
+        private DefaultMountPointInfo(String path, IRequestTargetUrlCodingStrategy codingStrategy) {
+            validateNotEmpty(path, "path");
+            validateNotNull(codingStrategy, "codingStrategy");
+            this.path = path;
+            this.codingStrategy = codingStrategy;
         }
 
-        public final String getPath()
-        {
-            return m_path;
+        public final String getPath() {
+            return path;
         }
 
-        public final IRequestTargetUrlCodingStrategy getCodingStrategy()
-        {
-            return m_codingStrategy;
+        public final IRequestTargetUrlCodingStrategy getCodingStrategy() {
+            return codingStrategy;
         }
     }
 }
