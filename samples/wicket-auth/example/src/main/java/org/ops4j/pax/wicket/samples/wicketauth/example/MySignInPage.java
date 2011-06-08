5d6c867c8b50b1a1aed1ce0839f78d4cdd0f8d36
Andreas Pieber <anpieber@gmail.com>
2011-06-08 15:11:45 +0200

    [PAXWICKET-108] Updated all licenses
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

--- samples/wicket-auth/example/src/main/java/org/ops4j/pax/wicket/samples/wicketauth/example/MySignInPage.java
+++ samples/wicket-auth/example/src/main/java/org/ops4j/pax/wicket/samples/wicketauth/example/MySignInPage.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright OPS4J
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /*
  * $Id: MySignInPage.java 459307 2006-02-14 09:57:21Z jonl $
  * $Revision: 459307 $ $Date: 2006-02-14 18:57:21 +0900 (Tue, 14 Feb 2006) $
--- samples/wicket-auth/example/src/main/java/org/ops4j/pax/wicket/samples/wicketauth/example/UserRolesAuthorizer.java
+++ samples/wicket-auth/example/src/main/java/org/ops4j/pax/wicket/samples/wicketauth/example/UserRolesAuthorizer.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright OPS4J
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /*
  * $Id: UserRolesAuthorizer.java 459160 2006-02-11 06:14:33Z jonl $ $Revision: 459160 $ $Date: 2006-02-11 15:14:33 +0900 (Sat, 11 Feb 2006) $
  * 
