dfc3329ddf33e666b6e72902dda02d61881212b8
Andreas Pieber <anpieber@gmail.com>
2011-06-10 08:46:07 +0200

    [PAXWICKET-133] Move BundleClassResolverHelper one level up
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

--- service/src/main/java/org/ops4j/pax/wicket/util/BundleClassResolverHelper.java
+++ service/src/main/java/org/ops4j/pax/wicket/util/BundleClassResolverHelper.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.ops4j.pax.wicket.util.classResolver;
+package org.ops4j.pax.wicket.util;
 
 import static org.ops4j.lang.NullArgumentException.validateNotNull;
 import static org.ops4j.pax.wicket.api.ContentSource.APPLICATION_NAME;
