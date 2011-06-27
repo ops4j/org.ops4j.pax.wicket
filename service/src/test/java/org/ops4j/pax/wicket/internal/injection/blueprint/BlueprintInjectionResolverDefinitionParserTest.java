366554cf6f6d3c837de3e1dd45032e7e4de214b6
Andreas Pieber <anpieber@gmail.com>
2011-06-27 09:53:27 +0200

    [PAXWICKET-110] Add InjectionResolver support for blueprint
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

---
+++ service/src/test/java/org/ops4j/pax/wicket/internal/injection/blueprint/BlueprintInjectionResolverDefinitionParserTest.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright OPS4J
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ops4j.pax.wicket.internal.injection.blueprint;
+
+import static org.hamcrest.Matchers.typeCompatibleWith;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.ops4j.pax.wicket.internal.injection.BundleInjectionProviderHelperDecorator;
+
+public class BlueprintInjectionResolverDefinitionParserTest {
+    @Test
+    public void testRequestRuntimeClass_shouldReturnApplicationDecorator() throws Exception {
+        BlueprintInjectionResolverDefinitionParser parserToTest = new BlueprintInjectionResolverDefinitionParser();
+
+        Class<?> runtimeClass = parserToTest.getRuntimeClass();
+
+        assertThat(runtimeClass, typeCompatibleWith(BundleInjectionProviderHelperDecorator.class));
+    }
+
+    @Test
+    public void testParse() throws Exception {
+        BlueprintParserTestUtil parserTestUtil =
+            new BlueprintParserTestUtil("wicket:injectionProvider", new BlueprintInjectionResolverDefinitionParser());
+
+        parserTestUtil.verifyId("injectionProvider");
+        parserTestUtil.verifyPropertyValue("applicationName");
+    }
+}
