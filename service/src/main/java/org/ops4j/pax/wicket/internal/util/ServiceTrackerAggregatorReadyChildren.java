f1d9d45a6bc3e8a508d091e04be6bf0ecb61ffcd
Andreas Pieber <anpieber@gmail.com>
2012-05-08 15:58:19 +0200

    [PAXWICKET-332] Make sure that IWebAppFact trackers do not kill each other
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>
    (cherry picked from commit 3622c47ac9054e6b1c880494f006666bd0889ac5)
    
    Conflicts:
    
    	service/src/main/java/org/ops4j/pax/wicket/internal/Activator.java
    
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

---
+++ service/src/main/java/org/ops4j/pax/wicket/internal/util/ServiceTrackerAggregatorReadyChildren.java
@@ -0,0 +1,29 @@
+package org.ops4j.pax.wicket.internal.util;
+
+import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTracker;
+
+/**
+ * Since a service tracker is not directly aggregatorable. Therefore this interface slightly modifies the functionality
+ * to make it usable.
+ */
+public interface ServiceTrackerAggregatorReadyChildren<ServiceType> {
+
+    /**
+     * Almost like the addingService method of the ServiceTracker but already including the service and without a return
+     * value.
+     */
+    void addingService(ServiceReference reference, ServiceType service);
+
+    /**
+     * Almost the same like the {@link #modifiedService(ServiceReference, Object)} method of the {@link ServiceTracker}
+     * without a super mehtod.
+     */
+    public void modifiedService(ServiceReference reference, ServiceType service);
+
+    /**
+     * Almost the same like the {@link #removedService(ServiceReference, Object)} method of the {@link ServiceTracker}
+     * without a super mehtod.
+     */
+    public void removedService(ServiceReference reference, ServiceType service);
+}
