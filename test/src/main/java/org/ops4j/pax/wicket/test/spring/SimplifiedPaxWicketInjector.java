30f2822a8662f0cfbe0e100d0235e70b9bbd3528
Andreas Pieber <anpieber@gmail.com>
2012-08-27 06:47:34 +0200

    [PAXWICKET-342] Added a helper to reduce the required code to register beans
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>
    (cherry picked from commit 0237aeddd0e40d78ef41f4d88b26d3c44b02a617)
    
    Signed-off-by: Andreas Pieber <anpieber@gmail.com>

---
+++ test/src/main/java/org/ops4j/pax/wicket/test/spring/SimplifiedPaxWicketInjector.java
@@ -0,0 +1,25 @@
+package org.ops4j.pax.wicket.test.spring;
+
+import org.apache.wicket.util.tester.WicketTester;
+
+/**
+ * An injector using the ApplicationContextMock and the PaxWicketSpringBeanComponentInjector in a quite simplified
+ * way making working based on them easier.
+ */
+public class SimplifiedPaxWicketInjector {
+    private final ApplicationContextMock applicationContext = new ApplicationContextMock();
+
+    public static SimplifiedPaxWicketInjector registerBeanInjector(WicketTester tester) {
+        return new SimplifiedPaxWicketInjector(tester);
+    }
+
+    private SimplifiedPaxWicketInjector(WicketTester tester) {
+        tester.getApplication().addComponentInstantiationListener(
+                new PaxWicketSpringBeanComponentInjector(tester.getApplication(), applicationContext));
+    }
+
+    public void registerBean(String name, Object bean) {
+        applicationContext.putBean(name, bean);
+    }
+
+}
