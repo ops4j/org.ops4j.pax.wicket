f9906721c542c6af4b2296c4d487941274e705c3
nmw <nmw@netdesign.dk>
2016-04-13 14:29:37 +0200

    closer to working test

--- org.ops4j.pax.wicket.itests.karaf/src/test/java/org/ops4j/pax/wicket/it/samples/karaf/SampleWebUiTest.java
+++ org.ops4j.pax.wicket.itests.karaf/src/test/java/org/ops4j/pax/wicket/it/samples/karaf/SampleWebUiTest.java
@@ -25,6 +25,8 @@ import org.ops4j.pax.exam.junit.PaxExam;
 
 import java.io.File;
 import javax.inject.Inject;
+import org.apache.wicket.protocol.http.WebApplication;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.ops4j.pax.exam.CoreOptions.maven;
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
@@ -33,10 +35,10 @@ import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.configure
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.keepRuntimeFolder;
-import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.logLevel;
-import org.ops4j.pax.exam.karaf.options.LogLevelOption.LogLevel;
 import org.ops4j.pax.exam.options.MavenArtifactUrlReference;
 import org.ops4j.pax.exam.options.MavenUrlReference;
+import org.ops4j.pax.exam.util.Filter;
+import org.ops4j.pax.wicket.api.WebApplicationFactory;
 import org.ops4j.pax.wicket.samples.plain.simple.service.EchoService;
 import org.osgi.framework.BundleContext;
 
@@ -58,20 +60,20 @@ public class SampleWebUiTest {
     @Inject
     private BundleContext bundleContext;
 
-//    @Inject
-//    @Filter(value = "(pax.wicket.applicationname=edge.inheritinjection)", timeout = TIMEOUT)
-//    private WebApplicationFactory<WebApplication> factoryEdgeInheritInjection;
+    @Inject
+    @Filter(value = "(pax.wicket.applicationname=edge.inheritinjection)", timeout = TIMEOUT)
+    private WebApplicationFactory<WebApplication> factoryEdgeInheritInjection;
 
-//    @Inject
-//    @Filter(value = "(pax.wicket.applicationname=springdm.simple.default)", timeout = TIMEOUT)
-//    private WebApplicationFactory<WebApplication> factorySpringDmSimpleDefault;
+    @Inject
+    @Filter(value = "(pax.wicket.applicationname=springdm.simple.default)", timeout = TIMEOUT)
+    private WebApplicationFactory<WebApplication> factorySpringDmSimpleDefault;
 
     /**
      * see module: /samples/ds/webapplication
      */
-//    @Inject
-//    @Filter(value = "(pax.wicket.applicationname=sample.ds.factory)", timeout = TIMEOUT)
-//    private WebApplicationFactory<WebApplication> factorySampleDS;
+    @Inject
+    @Filter(value = "(pax.wicket.applicationname=sample.ds.factory)", timeout = TIMEOUT)
+    private WebApplicationFactory<WebApplication> factorySampleDS;
 
     @Configuration
     public final Option[] configureAdditionalProvision() {
@@ -99,8 +101,7 @@ public class SampleWebUiTest {
             .unpackDirectory(new File("target", "exam"))
             .useDeployFolder(false),
             keepRuntimeFolder(),
-            configureConsole().ignoreLocalConsole(), logLevel(LogLevel.TRACE),
-            
+            configureConsole().ignoreLocalConsole(), //logLevel(LogLevel.TRACE),
 
             features(karafStandardRepo, "scr"),
             features(karafStandardRepo, "webconsole"),
@@ -122,108 +123,104 @@ public class SampleWebUiTest {
             features(karafSampleFeatureRepo, "wicket-samples-ds"),
             features(karafSampleFeatureRepo, "wicket-samples-edge-inheritinjection"),
 
-
-
-            
-
-             provision(mavenBundle().groupId("org.openengsb.wrapped").artifactId("net.sourceforge.htmlunit-all")
-                .versionAsInProject())
+            provision(mavenBundle().groupId("org.openengsb.wrapped").artifactId("net.sourceforge.htmlunit-all")
+            .versionAsInProject())
         };
 
     }
 
     @Test
     public void testIfAllExamplesWhereLoaded_shouldBeAbleToAccessThemAll() throws Exception {
-//        assertNotNull(factoryEdgeInheritInjection);
-//        assertNotNull(factorySpringDmSimpleDefault);
-//        assertNotNull(factorySampleDS);
+        assertNotNull(factoryEdgeInheritInjection);
+        assertNotNull(factorySpringDmSimpleDefault);
+        assertNotNull(factorySampleDS);
 //        //Register a service here for later injection
         bundleContext.registerService(EchoService.class, new EchoServiceImplementation(), null);
-
-        // testNavigationApplication_shouldRender
-        WebClient webclient = new WebClient();
-        HtmlPage page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/navigation/");
-        assertTrue(page.asText().contains("Homepage linking all OPS4J samples"));
-        webclient.closeAllWindows();
-        // testSamplePlainSimple_shouldRenderPage
-        webclient = new WebClient();
-        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/plain/simple/");
-        assertTrue(page.asText().contains("Welcome to the most simple pax-wicket application"));
-        webclient.closeAllWindows();
-        // testSamplePlainPageFactoryShouldAllowLink
-        webclient = new WebClient();
-        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/plain/pagefactory/");
-        assertTrue(page.asText().contains("Welcome to the most simple pax-wicket application"));
-        webclient.closeAllWindows();
-        //Check injected page
-        webclient = new WebClient();
-        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/plain/inject/");
-        assertTrue("/plain/inject/ failed to start properly", page.asText().contains("Echo: Welcome to the most simple pax-wicket application"));
-        webclient.closeAllWindows();
-        // testSampleBlueprintSimpleDefault_shouldRenderPage
-        webclient = new WebClient();
-        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/blueprint/simple/default");
-        assertTrue(page.asText().contains("Welcome to the most simple pax-wicket application based on blueprint"));
-        webclient.closeAllWindows();
-        // testSampleBlueprintSimplePaxwicket_shouldRenderPage
-        webclient = new WebClient();
-        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/blueprint/simple/paxwicket");
-        assertTrue(page.asText().contains("Welcome to the most simple pax-wicket application based on blueprint"));
-        webclient.closeAllWindows();
-        // testSampleBlueprintMountPoint_shouldRenderPage
-        webclient = new WebClient();
-        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/blueprint/mount/manuallymounted");
-        assertTrue(page.asText().contains("This page is mounted manually."));
-        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/blueprint/mount/automounted");
-        assertTrue(page.asText().contains("This page is automatically mounted."));
-        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/blueprint/mount/initiallymounted");
-        assertTrue(page.asText().contains("This page is mounted initially."));
-        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/blueprint/mount");
-        assertTrue(page.asText().contains("Mountpoint blueprint based sample."));
-        webclient.closeAllWindows();
-        // testSampleBlueprintMountPoint_shouldRenderPage
-        webclient = new WebClient();
-        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/blueprint/applicationfactory/first");
-        assertTrue(page.asText().contains("This is the 'The first' application home page."));
-        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/blueprint/applicationfactory/second");
-        assertTrue(page.asText().contains("This is the 'The second' application home page."));
-        webclient.closeAllWindows();
-        // testSampleSpringdmSimpleDefault_shouldRenderPage
-        webclient = new WebClient();
-        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/springdm/simple/default");
-        assertTrue(page.asText().contains("Welcome to the most simple pax-wicket application based on springdm"));
-        webclient.closeAllWindows();
-        // testSampleSpringdmInjectionSimple_shouldRenderPage
-        webclient = new WebClient();
-        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/springdm/injection/simple");
-        assertTrue(page.asText().contains(
-                "Welcome to the most simple pax-wicket injection application based on springdm."));
-        webclient.closeAllWindows();
-        // testSampleSpringdmSimplePaxwicket_shouldRenderPage
-        webclient = new WebClient();
-        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/springdm/simple/paxwicket");
-        assertTrue(page.asText().contains("Welcome to the most simple pax-wicket application based on springdm"));
-        webclient.closeAllWindows();
-        // testSampleMixed_shouldRenderPage
-        webclient = new WebClient();
-        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/mixed");
-        assertTrue(page.asText().contains(
-                "Welcome to the mixed component and technology example. Enjoy the full power of pax wicket!."));
-        assertTrue(page.asText().contains("This is a link"));
-        assertTrue(page.asText().contains("This is a panel from a separate component"));
-        webclient.closeAllWindows();
-        // testSampleEdgeInheritInjection_shouldRenderPage
-        webclient = new WebClient();
-        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/edge/inheritinjection");
-        assertTrue(page.asText().contains("Back to parent"));
-        assertTrue(page.asText().contains("This is a link"));
-        webclient.closeAllWindows();
-
-        // declarative services
-        webclient = new WebClient();
-        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/example/ds");
-        assertTrue(page.asText().contains("Declarative Services"));
-        webclient.closeAllWindows();
+        System.in.read();
+//        // testNavigationApplication_shouldRender
+//        WebClient webclient = new WebClient();
+//        HtmlPage page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/navigation/");
+//        assertTrue(page.asText().contains("Homepage linking all OPS4J samples"));
+//        webclient.closeAllWindows();
+//        // testSamplePlainSimple_shouldRenderPage
+//        webclient = new WebClient();
+//        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/plain/simple/");
+//        assertTrue(page.asText().contains("Welcome to the most simple pax-wicket application"));
+//        webclient.closeAllWindows();
+//        // testSamplePlainPageFactoryShouldAllowLink
+//        webclient = new WebClient();
+//        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/plain/pagefactory/");
+//        assertTrue(page.asText().contains("Welcome to the most simple pax-wicket application"));
+//        webclient.closeAllWindows();
+//        //Check injected page
+//        webclient = new WebClient();
+//        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/plain/inject/");
+//        assertTrue("/plain/inject/ failed to start properly", page.asText().contains("Echo: Welcome to the most simple pax-wicket application"));
+//        webclient.closeAllWindows();
+//        // testSampleBlueprintSimpleDefault_shouldRenderPage
+//        webclient = new WebClient();
+//        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/blueprint/simple/default");
+//        assertTrue(page.asText().contains("Welcome to the most simple pax-wicket application based on blueprint"));
+//        webclient.closeAllWindows();
+//        // testSampleBlueprintSimplePaxwicket_shouldRenderPage
+//        webclient = new WebClient();
+//        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/blueprint/simple/paxwicket");
+//        assertTrue(page.asText().contains("Welcome to the most simple pax-wicket application based on blueprint"));
+//        webclient.closeAllWindows();
+//        // testSampleBlueprintMountPoint_shouldRenderPage
+//        webclient = new WebClient();
+//        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/blueprint/mount/manuallymounted");
+//        assertTrue(page.asText().contains("This page is mounted manually."));
+//        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/blueprint/mount/automounted");
+//        assertTrue(page.asText().contains("This page is automatically mounted."));
+//        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/blueprint/mount/initiallymounted");
+//        assertTrue(page.asText().contains("This page is mounted initially."));
+//        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/blueprint/mount");
+//        assertTrue(page.asText().contains("Mountpoint blueprint based sample."));
+//        webclient.closeAllWindows();
+//        // testSampleBlueprintMountPoint_shouldRenderPage
+//        webclient = new WebClient();
+//        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/blueprint/applicationfactory/first");
+//        assertTrue(page.asText().contains("This is the 'The first' application home page."));
+//        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/blueprint/applicationfactory/second");
+//        assertTrue(page.asText().contains("This is the 'The second' application home page."));
+//        webclient.closeAllWindows();
+//        // testSampleSpringdmSimpleDefault_shouldRenderPage
+//        webclient = new WebClient();
+//        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/springdm/simple/default");
+//        assertTrue(page.asText().contains("Welcome to the most simple pax-wicket application based on springdm"));
+//        webclient.closeAllWindows();
+//        // testSampleSpringdmInjectionSimple_shouldRenderPage
+//        webclient = new WebClient();
+//        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/springdm/injection/simple");
+//        assertTrue(page.asText().contains(
+//                "Welcome to the most simple pax-wicket injection application based on springdm."));
+//        webclient.closeAllWindows();
+//        // testSampleSpringdmSimplePaxwicket_shouldRenderPage
+//        webclient = new WebClient();
+//        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/springdm/simple/paxwicket");
+//        assertTrue(page.asText().contains("Welcome to the most simple pax-wicket application based on springdm"));
+//        webclient.closeAllWindows();
+//        // testSampleMixed_shouldRenderPage
+//        webclient = new WebClient();
+//        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/mixed");
+//        assertTrue(page.asText().contains(
+//                "Welcome to the mixed component and technology example. Enjoy the full power of pax wicket!."));
+//        assertTrue(page.asText().contains("This is a link"));
+//        assertTrue(page.asText().contains("This is a panel from a separate component"));
+//        webclient.closeAllWindows();
+//        // testSampleEdgeInheritInjection_shouldRenderPage
+//        webclient = new WebClient();
+//        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/edge/inheritinjection");
+//        assertTrue(page.asText().contains("Back to parent"));
+//        assertTrue(page.asText().contains("This is a link"));
+//        webclient.closeAllWindows();
+//
+//        // declarative services
+//        webclient = new WebClient();
+//        page = webclient.getPage("http://localhost:" + WEBUI_PORT + "/example/ds");
+//        assertTrue(page.asText().contains("Declarative Services"));
+//        webclient.closeAllWindows();
     }
 
     /**
