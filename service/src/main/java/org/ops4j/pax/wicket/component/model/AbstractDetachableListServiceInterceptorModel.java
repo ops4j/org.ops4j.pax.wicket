c8530e8d71892b271cbd9fcb9f823d04ada8c603
Martin Nybo Nielsen <mnn@netdesign.dk>
2016-04-27 15:30:26 +0200

    Added a new model, which returns a list from a service, and allows for the list to be modified after the load is complete.

---
+++ service/src/main/java/org/ops4j/pax/wicket/component/model/AbstractDetachableListServiceInterceptorModel.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright OPS4J
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.ops4j.pax.wicket.component.model;
+
+import java.util.List;
+
+/**
+ * This model makes it easier to work with OSGi services in Wicket. It is a LoadableDetachableModel that loads data via Services retrieved 
+ * from the Service Registry. The model takes a Service Type and, when load is called, calls {@code doLoad} on each service. When the list is loaded, 
+ * it is passed to the method {@code parseList}, where the list and contents can be modified before the list is returned. The list returned
+ * from load will contain the output of every service that did not return exceptions, modified by {@code parseList}.
+ * @author Martin Nybo Nielsen
+ * @param <T> The type of service to poll for populating the model.
+ * @param <E> The return type expected from the model.
+ */
+public abstract class AbstractDetachableListServiceInterceptorModel<T extends Object, E extends Object> extends AbstractDetachableListServiceModel<T, E>{
+
+    public AbstractDetachableListServiceInterceptorModel(Class<T> serviceType, Class owningBundleClass, String filter) {
+	super(serviceType, owningBundleClass, filter);
+    }
+
+    public AbstractDetachableListServiceInterceptorModel(Class<T> serviceType, Class owningBundleClass) {
+	super(serviceType, owningBundleClass);
+    }
+
+    public AbstractDetachableListServiceInterceptorModel(Class<T> serviceType, Object owningBundleObject, String filter) {
+	super(serviceType, owningBundleObject, filter);
+    }
+
+    public AbstractDetachableListServiceInterceptorModel(Class<T> serviceType, Object owningBundleObject) {
+	super(serviceType, owningBundleObject);
+    }
+
+    
+    
+    protected abstract void parseList(List<E> tpParse);
+    
+    @Override
+    protected List<E> load() {
+	List<E> loadedList = super.load();
+	parseList(loadedList);
+	return loadedList;
+    }
+
+    
+
+    
+}
